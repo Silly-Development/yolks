eba52937a7b372ae67beeac6b8663fee16aedc84
Gamer3514 <gamer@sillydev.co.uk>
2024-09-19 17:58:46 +0100

    Update entrypoint.sh

--- dotnet/entrypoint.sh
+++ dotnet/entrypoint.sh
@@ -16,5 +16,15 @@ dotnet --version
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
+# Donation Message
+echo -e "\e[32m"  # Set text to green
+echo "##############################################"
+echo "#                                            #"
+echo "#   Please consider donating to Silly Dev!   #"
+echo "#           https://bit.ly/4e3XUce           #"
+echo "#                                            #"
+echo "##############################################"
+echo -e "\e[0m"  # Reset text color to default
+
 # Run the Server
 eval ${MODIFIED_STARTUP}
