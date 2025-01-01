30eca21cc4f76b13afc7bbd3cbfeb7561c834997
Gamer3514 <gamer@sillydev.co.uk>
2025-01-01 19:52:19 +0000

    Update entrypoint.sh

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 cd /home/container
 
-# Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# Print Node.js Version
 node -v
 
-# Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 
-# Print the modified startup command
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server
 eval ${MODIFIED_STARTUP}
